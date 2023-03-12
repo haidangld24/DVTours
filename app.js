b02cac90b65f4d055cee63a9bbca26ac838abe25
haidangld24 <67532262+haidangld24@users.noreply.github.com>
2023-03-12 16:01:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://dvtours.onrender.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
